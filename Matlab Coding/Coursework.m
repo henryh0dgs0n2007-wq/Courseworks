029bbe26db5cdb9c4e80f22076113f50a9d0bb47
Henry <Henry@HenrysPC>
2025-12-22 13:50:46 +0000

    start

---
+++ Matlab Coding/Coursework.m
@@ -0,0 +1,57 @@
+% Henry_Hodgson_20752939.m. 
+% egyhh13@nottingham.ac.uk
+
+
+%% Q1 - VARIABLES [9 MARKS]
+x = 2.3
+% a)
+a = sin(3*x)
+% b)
+b = cos(2*x) + sin(2*x)
+% c)
+c = x^6 - 4*x^4 + 2*x^2 - 7
+% d)
+d = exp(sin(x)*cos(x))
+% e)
+e = (x+2)/(x-2)+(x^2+1)/(x^3)
+% f)
+f = sin(3*x)^2 + cos(x)^2 + sin(x)^2
+% g)
+e = sqrt(x^2 + 1)*x^3 + (x+2)/(x-2)
+%% Q2 - VECTORS 1 [9 MARKS]
+% a)  
+x = linspace(-3*pi, 3*pi, 150)
+% b)
+a = cos(x)
+% d) 
+exp(-cos(x).^2)
+
+
+%% Q3 - VECTORS 2 [7 MARKS]
+clear
+
+
+%% Q4 - EQUATIONS [11 MARKS]
+clear
+
+
+
+%% Q5 - TEXT FORMAT, PRINT AND LOOPS [12 MARKS]
+clear
+
+
+%% Q6 - PROGRAM FLOW [14 MARKS]
+clear
+
+
+
+%% Q7 - FOR LOOPS, SWITCH STATEMENTS AND DISPLAYING DATA [16 MARKS]
+clear
+
+
+
+%% PRESENTATION AND FORMATTING [15 MARKS]
+% You do not need to put anything in this section
+% Marks will be given for neat, tidy presentation, sensible and informative
+% variable names, commenting and remembering to put your name and email address at the top of the
+% script
